8fe8a41c85faea44f8bfdf8a95de7dc1b3051a02
maxwel kirimi <maxwelkirimii@gmail.com>
2024-12-02 11:43:26 +0300

    assignment

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-
-import ipdb
-
 def plus_two(num):
-    num + 2
+    num=num + 2  # This doesn't update num, it just adds 2 to it temporarily # Pause here to inspect and debug
     return num
+
